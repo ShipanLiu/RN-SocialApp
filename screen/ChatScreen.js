fc49ef6ed374a641f09f2123b1b661f0f74d6f1d
Shipan <liushipan1998@gmail.com>
2021-07-19 08:29:38 +0200

    some page added

---
+++ screen/ChatScreen.js
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import FormButton from '../components/FormButton';
+import {AuthContext} from '../navigation/AuthProvider';
+
+export default function HomeScreen(props) {
+  const {logout, user} = useContext(AuthContext);
+  return (
+    <View style={styles.container}>
+      <Text>ChatScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+});
